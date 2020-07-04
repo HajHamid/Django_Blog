f683138b1e6b6c4873b78a06fdf2550fa1dcad90
Hamid Gohari <hamid.goharich@gmail.com>
2020-07-04 18:04:56 +0430

    post model and first page of blog is done

--- 01 - Get start (Create model post)/django_project/blog/models.py
+++ 01 - Get start (Create model post)/django_project/blog/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    brief = models.CharField(max_length=300)
+    content = models.TextField()
+    image = models.ImageField(upload_to="post_pics/")
+    date_posted = models.DateField(default=timezone.now)
+    in_slider = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
