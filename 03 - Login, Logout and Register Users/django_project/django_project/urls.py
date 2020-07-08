8a5ba45245caec9d1e9ccc2d1581c443c55caa7a
Hamid Gohari <hamid.goharich@gmail.com>
2020-07-08 14:32:34 +0430

    Register user and Create Profile is done

--- 03 - Login, Logout and Register Users/django_project/django_project/urls.py
+++ 03 - Login, Logout and Register Users/django_project/django_project/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from users.forms import MyAuthForm
+from users import views as users_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -22,6 +23,22 @@ urlpatterns = [
         name="logout",
     ),
     path("auth/", include("social_django.urls", namespace="social")),
+    path("register/", users_views.register, name="register"),
+    path("profile/", users_views.profile, name="profile"),
+    path(
+        "password_change",
+        auth_views.PasswordChangeView.as_view(
+            template_name="registration/password_change.html"
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
 ]
 
 if settings.DEBUG:
